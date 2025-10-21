8146d4234863b3fd2d75658386735c6d0fb90f28
marcosnjunior-jpg <marcos.n.junior@ba.estudante.senai.br>
2025-10-20 21:04:11 -0300

    Add HomeController for handling home route

---
+++ src/main/java/com/techplus/sistema/controller/HomeController.java
@@ -0,0 +1,13 @@
+package c.techplus.sistema.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // nome da view
+    }
+}
