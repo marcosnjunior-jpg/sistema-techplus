3f522fe227c18f6ed92a5c8dfb696e239d7ebdba
marcosnjunior-jpg <marcos.n.junior@ba.estudante.senai.br>
2025-10-20 22:33:21 -0300

    Update HomeController.java

--- src/main/java/c/techplus/sistema/controller/HomeController.java
+++ src/main/java/c/techplus/sistema/controller/HomeController.java
@@ -1,15 +1,1 @@
-package com.techplus.sistema.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String index(Model model) {
-        model.addAttribute("mensagem", "Bem-vindo ao Sistema TechPlus!");
-        return "index"; // deve existir src/main/resources/templates/index.html
-    }
-}
