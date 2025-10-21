ee962861cd93df17d9bdbbd97050661821433a96
marcosnjunior-jpg <marcos.n.junior@ba.estudante.senai.br>
2025-10-20 23:00:34 -0300

    Add JPA annotations to Produto class

--- src/main/java/com/techplus/sistema/model/Produto.java
+++ src/main/java/com/techplus/sistema/model/Produto.java
@@ -1,6 +1,16 @@
 package com.techplus.sistema.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String nome;
     private String descricao;
@@ -20,6 +30,9 @@ public class Produto {
         this.preco = preco;
         this.quantidade = quantidade;
     }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
