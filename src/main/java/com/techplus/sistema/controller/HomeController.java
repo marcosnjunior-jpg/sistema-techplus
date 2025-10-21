dda02623a741153312793cfeb17e9e53c2597048
marcosnjunior-jpg <marcos.n.junior@ba.estudante.senai.br>
2025-10-20 23:23:58 -0300

    Change home view return from 'home' to 'index'

--- src/main/java/com/techplus/sistema/controller/HomeController.java
+++ src/main/java/com/techplus/sistema/controller/HomeController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/")           // <- mapeia a raiz "/"
     public String home() {
-        return "home"; // nome da view
+        return "index";        // <- nome do template sem extensão
     }
 }
