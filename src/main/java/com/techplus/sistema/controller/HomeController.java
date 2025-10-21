e52de65a58b1b6778954e5cf46e1c31a61aa08e3
marcosnjunior-jpg <marcos.n.junior@ba.estudante.senai.br>
2025-10-20 23:20:34 -0300

    Fix package declaration in HomeController.java

--- src/main/java/com/techplus/sistema/controller/HomeController.java
+++ src/main/java/com/techplus/sistema/controller/HomeController.java
@@ -1,4 +1,4 @@
-package c.techplus.sistema.controller;
+package com.techplus.sistema.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
